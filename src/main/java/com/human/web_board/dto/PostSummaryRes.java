c8bbf2787f41cfd7cdcd9efd800c18aa51a32b25
hallispio <bureum66@gmail.com>
2025-11-12 10:15:35 +0900

    Reapply "부름 회원가입(오류수정HTML NEW)"
    This reverts commit f6295e59b4f6242b207694596c9b22f2ade16813.

---
+++ src/main/java/com/human/web_board/dto/PostSummaryRes.java
@@ -0,0 +1,17 @@
+package com.human.web_board.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+// 게시글 응답
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class PostSummaryRes {
+    private Long id;
+    private String title;
+    private String category_name; // 카테고리 테이블에 있는 정보를 조인해서 가져 옴
+    private String nickname;  // 회원 테이블에 있는 정보를 조인해서 가져 옴
+    private Long view_count;
+    private Long recommendationsCount;
+    private LocalDateTime createdAt;
+}
