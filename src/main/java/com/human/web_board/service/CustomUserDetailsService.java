edbde47e5e0afd166bcc99e7de41550300dc0075
hallispio <bureum66@gmail.com>
2025-11-17 14:16:10 +0900

    로그인 되는 백엔드 일단 현재 완성(부름) html
    Signed-off-by: hallispio <bureum66@gmail.com>

---
+++ src/main/java/com/human/web_board/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.human.web_board.service;
+
+import com.human.web_board.dao.MemberDao;
+import com.human.web_board.dto.MemberRes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberDao memberDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        MemberRes member = memberDao.findByEmail(email);
+
+        if (member == null) {
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email);
+        }
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPwd())
+                .roles("USER")
+                .build();
+    }
+}
