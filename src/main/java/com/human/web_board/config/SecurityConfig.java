9ba914d1e27c6fdce8f06ede514264d3edb91be8
kurecat <ochant777@gmail.com>
2025-11-18 16:32:40 +0900

    글쓰기 페이지 헤더 푸터 이동 및 스마트에디터 적용 됨.

--- src/main/java/com/human/web_board/config/SecurityConfig.java
+++ src/main/java/com/human/web_board/config/SecurityConfig.java
@@ -6,8 +6,6 @@ import com.human.web_board.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,10 +22,14 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
 
+                .headers(headers -> headers
+                        .frameOptions(frameOptions -> frameOptions.sameOrigin())
+                )
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/css/**", "/js/**", "/images/**",
-                                "/webjars/**", "/favicon.ico"
+                                "/webjars/**", "/favicon.ico", "/smarteditor2/**"
                         ).permitAll()
                         .requestMatchers(
                                 "/", "/main", "/login",
@@ -74,16 +76,5 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-
-        authBuilder.userDetailsService(customUserDetailsService)
-                .passwordEncoder(passwordEncoder());
-
-        return authBuilder.build();
     }
 }
